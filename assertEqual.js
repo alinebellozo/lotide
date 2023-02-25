bc2e90a2d00909ea952d86ccae6dc5e688cad30d
Aline Bellozo <alinebellozo@gmail.com>
2023-02-25 02:11:01 +0000

    Practicing interpolation

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function (actual, expected) {
   if (actual !== expected) {
-    console.log(actual + " is different from " + expected);
+    console.log(`Oops, ${actual} is different from ${expected}`);
   } else {
-    console.log("Yay, " + actual + " is equal to " + expected);
+    console.log(`Yay, ${actual} is equal to ${expected}`);
   }
 };
 
