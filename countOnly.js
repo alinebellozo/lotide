eaafa0c5b53bfc66b4e38b9fdeacaaf48deae49d
Aline Bellozo <alinebellozo@gmail.com>
2023-03-04 04:59:54 +0000

    Implementing countOnly in Lotide

---
+++ countOnly.js
@@ -0,0 +1,67 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual !== expected) {
+    console.log(`Oops, ${actual} is different from ${expected}`);
+  } else {
+    console.log(`Yay, ${actual} is equal to ${expected}`);
+  }
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+
+// In order to fix the error, we can have our function define a new empty object called results and return it
+const countOnly = function (allItems, itemsToCount) {
+  const results = {};
+
+  // Increment the count of each item into results as we encounter each string item in the array.
+  for (const item of allItems) {
+    // inside the loop,
+    // increment the counter for each item:
+    //   set a property with that string key to:
+    //     the value that was already there (or zero if nothing there) with 1 added to it.
+
+    // if (results[item] === allItems) {
+    //   results[item] += 1;
+    // } else {
+    //   results[item] = 1;
+    // }
+
+    // Add an if condition to only increment our count in results if the item is found in the itemsToCount object.
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  console.log(results);
+  return results;
+};
+
+// TEST CODE
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false,
+});
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
