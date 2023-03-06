51730c2015392426e9e7e09d0cc59daa6801b991
Aline Bellozo <alinebellozo@gmail.com>
2023-03-06 00:57:31 +0000

    Imeplementing letter positions

---
+++ letterPositions.js
@@ -0,0 +1,44 @@
+// Implement a new function letterPositions which will return all the indices (zero-based positions) in the string where each character is found.
+
+// For each letter, instead of returning just one number to represent its number of occurrences, multiple numbers may be needed to represent all the places in the string that it shows up.
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`${arr1} matches ${arr2}`);
+  } else {
+    console.log(`${arr1} does not match ${arr2}`);
+  }
+};
+
+const letterPositions = function (sentence) {
+  const results = {};
+
+  for (let i = 0; i < sentence.length; i++) {
+    // check if the index is not empty
+    if (sentence[i] !== " ") {
+      // condition:
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+// assertArraysEqual(letterPositions("hello"), [0]); // => fail
+console.log(letterPositions("If nothing goes right, go left"));
