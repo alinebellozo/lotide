f82bd39b9a25b209c437c6d075f89f550106d308
Aline Bellozo <alinebellozo@gmail.com>
2023-03-08 19:56:36 +0000

    Implementing Map

---
+++ map.js
@@ -0,0 +1,44 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`Assertion passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`Assertion failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+// Call map and pass in our words and a simple arrow function.
+
+const map = function (array, callback) {
+  // Start off with an empty array and return that array
+  const results = [];
+
+  // Add the loop logic which just console.logs each item in the provided array as well as what happens to the item after being passed into the callback.
+  for (let item of array) {
+    results.push(callback(item));
+  }
+
+  return results;
+};
+
+const results1 = map(words, (word) => word[0]);
+console.log(results1);
+
+assertArraysEqual(
+  map(words, (word) => word[0]),
+  ["g", "c", "t", "m", "t"]
+);
