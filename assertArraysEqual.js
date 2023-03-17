a86f191315369d1d25d23364ebacbeb34229f80f
Aline Bellozo <alinebellozo@gmail.com>
2023-03-17 03:46:11 +0000

    Refactored assertArraysEqual file, and added assertArraysEqualTest file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,6 @@
 // Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
 // Instead of doing a simple === check though, this assertion function will make use of your eqArrays function for array comparison.
-
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function (arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
@@ -22,9 +10,4 @@ const assertArraysEqual = function (arr1, arr2) {
   }
 };
 
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => pass
-assertArraysEqual(eqArrays([1, 2, 3], [2, 3, 4]), false); // => pass
-assertArraysEqual(eqArrays(["1", "2", "3"], [2, 3, 4]), false); // => pass
-assertArraysEqual(eqArrays(["1", 2, "3"], [2, 3, 4]), false); // => pass
-assertArraysEqual(eqArrays([4, 5, 6], [4, 5, 6]), true); // => pass
-assertArraysEqual(eqArrays([7, 8], [7, 8, 9]), false); // => pass
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,11 @@
+const assertArraysEqual = require("../assertArraysEqual");
+const eqArrays = require("../eqArrays");
+
+// TEST CODE
+
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => pass
+assertArraysEqual(eqArrays([1, 2, 3], [2, 3, 4]), false); // => pass
+assertArraysEqual(eqArrays(["1", "2", "3"], [2, 3, 4]), false); // => pass
+assertArraysEqual(eqArrays(["1", 2, "3"], [2, 3, 4]), false); // => pass
+assertArraysEqual(eqArrays([4, 5, 6], [4, 5, 6]), true); // => pass
+assertArraysEqual(eqArrays([7, 8], [7, 8, 9]), false); // => pass
